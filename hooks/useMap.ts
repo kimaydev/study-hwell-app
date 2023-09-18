4f78705c585499b5f7733b6657ab6877e54ae825
kimaydev <kimaydev@gmail.com>
2023-09-18 12:40:04 +0900

    feat: json 파일 로드 완료

--- hooks/useMap.ts
+++ hooks/useMap.ts
@@ -3,3 +3,6 @@ import { Coordinates } from '@/types/info';
 export const INITIAL_CENTER: Coordinates = [37.3595704, 127.105399];
 export const INITIAL_ZOOM = 10;
 export const INITIAL_MIN = 6;
+
+const useMap = () => {};
+export default useMap;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,30 @@
+import { useEffect } from 'react';
 import Header from '@/components/common/Header';
 import MapScene from '@/components/home/MapScene';
+import { Info } from '@/types/info';
+interface IProps {
+  infos: Info[];
+}
 
-export default function Home() {
+export default function Home({ infos }: IProps) {
+  useEffect(() => {
+    console.log(infos);
+  }, [infos]);
+
   return (
     <>
       <Header />
       <MapScene />
     </>
   );
+}
+
+// pre-rendering 하여서 SSG 생성
+export async function getStaticProps() {
+  // public에 있는 info.json 가져오기
+  const infos = (await import('@/public/info.json')).default;
+  return {
+    props: { infos },
+    revalidate: 3600,
+  };
 }
